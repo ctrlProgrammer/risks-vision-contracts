9564903072d8a01cd54bbfc87084ae665217a504
ctrlprogrammer <sebastianto1999@gmail.com>
2026-04-12 05:32:41 -0500

    Update subscriptions

--- contracts/subscriptions.go
+++ contracts/subscriptions.go
@@ -1,10 +1,13 @@
 package contracts
 
 type Subscription struct {
-	ID         uint    `json:"id" gorm:"primaryKey"`
-	Slug       string  `json:"slug" gorm:"uniqueIndex"`
-	MaxAICalls int     `json:"max_ai_calls" gorm:"default:0"`
-	Price      float64 `json:"price" gorm:"default:0"`
+	ID            uint     `json:"id" gorm:"primaryKey"`
+	Slug          string   `json:"slug" gorm:"uniqueIndex"`
+	MaxAICalls    int      `json:"max_ai_calls" gorm:"default:0"`
+	Price         float64  `json:"price" gorm:"default:0"`
+	WhatsIncluded []string `json:"whats_included" gorm:"type:jsonb"`
+	Limitations   []string `json:"limitations" gorm:"type:jsonb"`
+	FrontName     string   `json:"front_name" gorm:"not null"`
 }
 
 func (s *Subscription) ToToon() string {
