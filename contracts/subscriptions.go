4ed1ffe379b857baeef2b38bb95eb445965460ed
ctrlprogrammer <sebastianto1999@gmail.com>
2026-04-12 06:04:06 -0500

    Add token bundles

--- contracts/subscriptions.go
+++ contracts/subscriptions.go
@@ -1,5 +1,7 @@
 package contracts
 
+import "time"
+
 type Subscription struct {
 	ID            uint     `json:"id" gorm:"primaryKey"`
 	Slug          string   `json:"slug" gorm:"uniqueIndex"`
@@ -7,9 +9,17 @@ type Subscription struct {
 	Price         float64  `json:"price" gorm:"default:0"`
 	WhatsIncluded []string `json:"whats_included" gorm:"type:jsonb"`
 	Limitations   []string `json:"limitations" gorm:"type:jsonb"`
-	FrontName     string   `json:"front_name" gorm:"not null"`
+	FrontName     *string  `json:"front_name" gorm:"not null"`
 }
 
 func (s *Subscription) ToToon() string {
 	return ToToon(s)
+}
+
+type TokenBundles struct {
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	AmountOfTokens int       `json:"amount_of_tokens" gorm:"default:0"`
+	Price          float64   `json:"price" gorm:"default:0"`
+	CreatedAt      time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt      time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
